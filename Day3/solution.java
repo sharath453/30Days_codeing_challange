1e051e368d49c00298e1ea166e0cfee56768e347
sharath <alsharath66@gmail.com>
2024-09-06 19:29:55 +0530

    Day3 solution

---
+++ Day3/solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+
+        // Check the conditions for the given integer N
+        if (N % 2 != 0) {
+            // N is odd
+            System.out.println("Weird");
+        } else {
+            // N is even
+            if (N >= 2 && N <= 5) {
+                System.out.println("Not Weird");
+            } else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            } else if (N > 20) {
+                System.out.println("Not Weird");
+            }
+        }
+    }
+}
