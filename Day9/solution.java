5ec883ae652d59697ae713c8d3a4b7e94666ffd0
sharath <alsharath66@gmail.com>
2024-09-12 18:49:06 +0530

    Day9

--- Day9/solution.java
+++ Day9/solution.java
@@ -8,21 +8,14 @@ import java.util.regex.*;
 
 class Result {
 
-    /*
-     * Complete the 'factorial' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER n as parameter.
-     */
-
     public static int factorial(int n) {
     // Write your code here
-    int result=1;
-    for(int i=1;i<=n;i++)
+    int results=1;
+    for(int m=1;m<=n;m++)
     {
-        result=result*i;
+        results=results*m;
     }
-    return result;
+    return results;
 
     }
 
