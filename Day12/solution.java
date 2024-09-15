ddb2f77055b2d812213b0fd10f4b0ffcac79a6a4
sharath <alsharath66@gmail.com>
2024-09-15 12:45:24 +0530

    day12

---
+++ Day12/solution.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+
+
+class Student extends Person{
+	private int[] testScores;
+
+Student(String firstName, String lastName, int id, int[] scores) {
+        super(firstName, lastName, id);  // Initialize base class
+        this.testScores = scores;  // Initialize Student's testScores
+    }
+
+    /*  
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    char calculate() {
+        // Calculate the average score
+        int sum = 0;
+        for (int score : testScores) {
+            sum += score;
+        }
+        double average = sum / (double) testScores.length;
+
+        // Determine the grade based on the average
+        if (average >= 90) {
+            return 'O';  // Outstanding
+        } else if (average >= 80) {
+            return 'E';  // Excellent
+        } else if (average >= 70) {
+            return 'A';  // Average
+        } else if (average >= 55) {
+            return 'P';  // Pass
+        } else if (average >= 40) {
+            return 'D';  // Fail
+        } else {
+            return 'T';  // Trolled (custom grade, not part of the standard grading scheme)
+        }
+    }
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate());
+	}
+}
