d6fac03f1e57f40b37bfa197843d48e48ac672c5
sharath <alsharath66@gmail.com>
2024-09-19 22:35:36 +0530

    Day17

--- Day17/solution.java
+++ Day17/solution.java
@@ -2,12 +2,13 @@ import java.util.*;
 import java.io.*;
 
 class Calculator{
-    
+
     public int power(int n,int p)throws Exception{
         if(n<0|| p<0)
         {
             throw new Exception("n and p should be non-negative");
         }
+        
         int result=(int) Math.pow(n, p);
         return result;
     }
