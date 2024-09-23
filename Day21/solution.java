8c9757514f4422acc296c6af052d7fb4ebc79aa6
sharath <alsharath66@gmail.com>
2024-09-23 22:09:36 +0530

    Day21

--- Day21/solution.java
+++ Day21/solution.java
@@ -1,12 +1,6 @@
 import java.util.*;
 
 class Printer <T> {
-
-    /**
-    *    Method Name: printArray
-    *    Print each element of the generic array on a new line. Do not return anything.
-    *    @param A generic array
-    **/
     
     // Write your code here
 public static <T> void printArray(T[] array) {
